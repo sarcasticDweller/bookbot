be66947e73886d4c54ee2618d0b9f543ab654259
sarcasticDweller <capslocked.strife@gmail.com>
2025-03-30 14:56:00 -0400

    CH2L4

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
 #!/opt/homebrew/bin/python3
 # the above shebang means i can run this script (on my mac, anyway) by just typing `./main.py` in the terminal.
 
-print("greetings boots")
+def get_book_text(file_path):
+    """
+    Reads the content of a book file and returns it as a string.
+    
+    :param file_path: Path to the book file.
+    :return: Content of the book as a string.
+    """
+    contents = ""
+    with open(file_path, 'r', encoding='utf-8') as file:
+        contents = file.read()
+        file.close()
+    return contents
+
+def get_book_wordcount(book_text):
+    """
+    Counts the number of words in the book text.
+    
+    :param book_text: The text of the book.
+    :return: Word count of the book text.
+    """
+    word_count = len(book_text.split())
+    return word_count
+
+def main():
+    """
+    Main function to execute the script.
+    """
+    path_to_book = "books/frankenstein.txt"
+    book_text = get_book_text(path_to_book)
+    book_word_count = get_book_wordcount(book_text)
+    print(f"{book_word_count} words found in the document")
+
+main()
