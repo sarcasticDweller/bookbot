7d244356abb5888a3e36fa3d29f66742555d703e
sarcasticDweller <capslocked.strife@gmail.com>
2025-03-30 16:10:52 -0400

    Should be done now

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 #!/opt/homebrew/bin/python3
 # the above shebang means i can run this script (on my mac, anyway) by just typing `./main.py` in the terminal.
+
+# python libraries
+import sys
+# project libraries
 import stats
+def get_book():
+    """
+    Reads the first argument and outputs it.
+
+    :return: Content of second system argument.
+    """
+    try:
+        return sys.argv[1]
+    except IndexError:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 def get_book_text(file_path):
     """
@@ -19,7 +34,7 @@ def main():
     """
     Main function to execute the script.
     """
-    path_to_book = "books/frankenstein.txt"
+    path_to_book = get_book() 
     book_text = get_book_text(path_to_book)
     book_word_count = stats.get_book_word_count(book_text)
     book_character_count = stats.sort_character_count(stats.get_character_count(book_text))
